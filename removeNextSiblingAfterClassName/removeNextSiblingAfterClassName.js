df1076d013444f8dc42c79153bfa34f26cd38683
Brian Swisher <eswish+brian@gmail.com>
2011-05-20 15:23:52 -0700

    adding consoleScript removeNextSiblingAfterClassName

---
+++ removeNextSiblingAfterClassName/removeNextSiblingAfterClassName.js
@@ -0,0 +1,25 @@
+/* removeNextSiblingAfterClassName - A script to remove the next sibling node after a given className.
+ * Copyright 2011, Brian Swisher, brianswisher.com
+ * @param className {String}
+ * @return {Array<node>} removed nodes || {Int} -1 if incompatible
+ */
+/*global document */
+(function(className){
+	var i, nodes, node, next, siblings = [], returnValue;
+	if (document.getElementsByClassName) {
+		nodes = document.getElementsByClassName(className);
+		for (i in nodes){
+			if (nodes.hasOwnProperty(i)) {
+				node = nodes[i];
+				if (node.nextSibling) {
+					next = node.nextSibling;
+					siblings.push(next.parentNode.removeChild(next));
+				}
+			}
+		}
+		returnValue = siblings;
+	} else {
+		returnValue = -1;
+	}
+	return returnValue;
+}("CLASS_NAME"));
